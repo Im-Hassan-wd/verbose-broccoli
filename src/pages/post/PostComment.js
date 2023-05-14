4013599fe7f9b62d0efd8ea67350271f9e675c05
Im-Hassan-wd <alhassansahih@gmail.com>
2023-05-15 00:36:45 +0100

    post comments

--- src/pages/post/PostComment.js
+++ src/pages/post/PostComment.js
@@ -3,19 +3,32 @@ import Avatar from "../../components/Avatar";
 export default function PostComment({ post }) {
   return (
     <ul className="post-comments">
+      <h4>
+        <span>All comments</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+          />
+        </svg>
+      </h4>
       {post.comments.length > 0 &&
         post.comments.map((comment) => (
           <li key={comment.id}>
             <div className="comment-author">
               <Avatar src={comment.photoURL} />
-              <p>{comment.displayName}</p>
+              <p>{comment.displayName}:</p>
+              <span>{comment.content}</span>
             </div>
-            <div className="comment-date">
-              <p>Date here</p>
-            </div>
-            <div className="comment-content">
-              <p>{comment.content}</p>
-            </div>
+            <div className="comment date">date here</div>
           </li>
         ))}
     </ul>
