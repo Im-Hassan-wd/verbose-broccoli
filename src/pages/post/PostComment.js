ed3735689530b38e36030502cf88bb1c17334b04
Im-Hassan-wd <alhassansahih@gmail.com>
2023-05-15 15:12:37 +0100

    post comments

--- src/pages/post/PostComment.js
+++ src/pages/post/PostComment.js
@@ -4,23 +4,27 @@ import formatDistanceToNow from "date-fns/formatDistanceToNow";
 export default function PostComment({ post }) {
   return (
     <ul className="post-comments">
-      <h4>
-        <span>All comments</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-          />
-        </svg>
-      </h4>
+      {post.comments.length > 0 ? (
+        <h4>
+          <span>All comments</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </h4>
+      ) : (
+        <h4>No comments yet</h4>
+      )}
       {post.comments.length > 0 &&
         post.comments.map((comment) => (
           <li key={comment.id}>
