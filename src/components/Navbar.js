2474bde20f441c4ce9890141994a048187cbb439
Im-Hassan-wd <alhassansahih@gmail.com>
2023-05-18 18:54:25 +0100

    burger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,22 @@ export default function Navbar({ screenWidth, mobileMenu, setMobileMenu }) {
   return (
     <nav className={`${mode}`}>
       {!mobileMenu && screenWidth < 549 && (
-        <button onClick={() => setMobileMenu(true)}>Burger</button>
+        <button className="burger" onClick={() => setMobileMenu(true)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 9h16.5m-16.5 6.75h16.5"
+            />
+          </svg>
+        </button>
       )}
       <div className="logo">
         <svg
