320bc757338ae5db166f49046fe30655a94217d1
Im-Hassan-wd <alhassansahih@gmail.com>
2023-05-22 21:27:09 +0100

    search term

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,6 +1,32 @@
+import { useLocation } from "react-router-dom";
+
 // styles
 import "./Search.css";
 
+// hooks and components
+import { useCollection } from "../../hooks/useCollection";
+import { useEffect } from "react";
+import PostList from "../../components/PostList";
+
 export default function Search() {
-  return <div>Search</div>;
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString);
+  const query = queryParams.get("q");
+
+  const { documents, error } = useCollection("posts");
+
+  const posts =
+    documents &&
+    documents.filter(
+      (doc) =>
+        doc.content.toLowerCase().includes(query.toLocaleLowerCase()) ||
+        doc.author.displayName.toLowerCase().includes(query.toLocaleLowerCase())
+    );
+
+  return (
+    <div className="search">
+      <h2></h2>
+      {posts && <PostList posts={posts} />}
+    </div>
+  );
 }
