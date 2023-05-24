595c196b0d9fd8f09e98b19778a62468fee963e0
Im-Hassan-wd <alhassansahih@gmail.com>
2023-05-24 18:26:44 +0100

    styling the profile page

--- src/components/Reaction.js
+++ src/components/Reaction.js
@@ -102,9 +102,10 @@ export default function Reaction({ post }) {
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+            d="M15 15l6-6m0 0l-6-6m6 6H9a6 6 0 000 12h3"
           />
         </svg>
+
         <span className="r-span">Share</span>
         <span className="count">0</span>
       </button>
--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -9,36 +9,39 @@ export default function Profile() {
   const { user } = useAuthContext();
   return (
     <div className="profile">
-      <div className="banner" />
-      <Avatar src={user.photoURL} />
+      <div className="banner">
+        <Avatar src={user.photoURL} />
+      </div>
+
       <div className="info">
         <h2>Amanda Smith</h2>
         <p>Nigeria</p>
-        <div className="">
-          <span>@amanda21</span>
-          <span>. Lead product designer</span>
-        </div>
-      </div>
-      <div className="btns">
-        <button>Message</button>
-        <button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15 15l6-6m0 0l-6-6m6 6H9a6 6 0 000 12h3"
-            />
-          </svg>
+        <ul className="">
+          <li>@amanda21</li>
+          <li>. Lead product designer</li>
+        </ul>
 
-          <span>Share profile</span>
-        </button>
+        <div className="btns">
+          <button>Message</button>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 15l6-6m0 0l-6-6m6 6H9a6 6 0 000 12h3"
+              />
+            </svg>
+
+            <span>Share profile</span>
+          </button>
+        </div>
       </div>
     </div>
   );
