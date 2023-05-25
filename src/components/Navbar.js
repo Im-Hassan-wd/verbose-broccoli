3071819e04159744966db5a5ea9213682e5b243d
Im-Hassan-wd <alhassansahih@gmail.com>
2023-05-25 06:52:15 +0100

    navbar option component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,14 @@ import { NavLink, Link } from "react-router-dom";
 
 // components and hooks
 import { useAuthContext } from "../hooks/useAuthContext";
-import { useLogout } from "../hooks/useLogout";
-import { useTheme } from "../hooks/useTheme";
 import Avatar from "./Avatar";
 
 // styles
 import "./Navbar.css";
+import NavbarOption from "./NavbarOption";
 
 export default function Navbar({ screenWidth, mobileMenu, setMobileMenu }) {
-  const { logout, isPending } = useLogout();
   const { user } = useAuthContext();
-  const { mode } = useTheme();
 
   return (
     <nav>
@@ -62,38 +59,7 @@ export default function Navbar({ screenWidth, mobileMenu, setMobileMenu }) {
         </li>
       </ul>
       <div className="more">
-        <button className="action">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"
-            />
-          </svg>
-        </button>
-        <button className="action" onClick={logout}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
-            />
-          </svg>
-        </button>
+        {screenWidth > 404 && <NavbarOption />}
         <Link to="/profile" className="user">
           <span>{user.displayName}</span>
           <Avatar src={user.photoURL} />
