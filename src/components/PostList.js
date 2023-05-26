7fb3ec048773a285be5b1395814bf66e2863d6fe
Im-Hassan-wd <alhassansahih@gmail.com>
2023-05-26 20:26:14 +0100

    flaticon

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -67,24 +67,17 @@ export default function PostList({ posts, msg }) {
                 {post.createdAt.toDate().toDateString().slice(3)}
               </span>
             </li>
-            {options && <Options />}
+            <button className="icon-btn">
+              <i className="fi fi-rr-pencil"></i>
+            </button>
+            <button className="icon-btn">
+              <i class="fi fi-rr-circle-cross"></i>
+            </button>
             {user.uid !== post.author.id && <BookmarkIcon post={post} />}
             {user.uid === post.author.id && (
-              <svg
-                onClick={() => setOptions(!options)}
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
-                />
-              </svg>
+              <button className="icon-btn" onClick={() => setOptions(!options)}>
+                <i className="fi fi-sr-menu-dots-vertical"></i>
+              </button>
             )}
           </div>
           <div onClick={() => handleClick(post)}>
