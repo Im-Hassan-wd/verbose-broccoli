069fe9d89d6548bcdefa6390fd26fc033ee602d1
Im-Hassan-wd <alhassansahih@gmail.com>
2023-05-26 20:31:19 +0100

    flaticon

--- src/components/BookmarkIcon.js
+++ src/components/BookmarkIcon.js
@@ -30,13 +30,10 @@ export default function BookmarkIcon({ post }) {
   };
 
   // post bookmarked by the current user
-  const bookmarkedPost = () => {
-    return (
-      post.bookmarks &&
-      post.bookmarks.filter((bookmark) => bookmark.uid === user.uid)
-    );
-  };
-  const bookmarked = bookmarkedPost();
+
+  const bookmarked =
+    post.bookmarks &&
+    post.bookmarks.filter((bookmark) => bookmark.uid === user.uid);
 
   const handleClick = async () => {
     if (bookmarked.length && bookmarked[0].uid === user.uid) {
@@ -53,24 +50,8 @@ export default function BookmarkIcon({ post }) {
   };
 
   return (
-    <svg
-      onClick={handleClick}
-      xmlns="http://www.w3.org/2000/svg"
-      fill={
-        bookmarked.length && bookmarked[0].uid === user.uid
-          ? "currentColor"
-          : "none"
-      }
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="w-6 h-6"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
-      />
-    </svg>
+    <button className="icon-btn">
+      <i class="fi fi-rr-bookmark"></i>
+    </button>
   );
 }
