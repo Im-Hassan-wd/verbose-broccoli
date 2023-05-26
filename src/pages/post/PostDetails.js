8d76c016c626164db40ea00f86abb97282e1e4e2
Im-Hassan-wd <alhassansahih@gmail.com>
2023-05-26 22:12:06 +0100

    post list component

--- src/pages/post/PostDetails.js
+++ src/pages/post/PostDetails.js
@@ -19,20 +19,9 @@ export default function PostDetails({ post }) {
             </span>
           </li>
           <BookmarkIcon post={post} />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
-            />
-          </svg>
+          <button className="icon-btn">
+            <i className="fi fi-sr-menu-dots-vertical"></i>
+          </button>
         </div>
         <p
           className="post-content"
