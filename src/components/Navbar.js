8cdf3b58ed4bd386f38c331d2e53976b6c5e76c6
Im-Hassan-wd <alhassansahih@gmail.com>
2023-05-26 22:44:34 +0100

    icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,20 +15,7 @@ export default function Navbar({ screenWidth, mobileMenu, setMobileMenu }) {
     <nav className="navbar">
       {!mobileMenu && screenWidth < 549 && (
         <button className="burger" onClick={() => setMobileMenu(true)}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 9h16.5m-16.5 6.75h16.5"
-            />
-          </svg>
+          <i class="fi fi-rr-bars-sort"></i>
         </button>
       )}
       <div className="logo">
