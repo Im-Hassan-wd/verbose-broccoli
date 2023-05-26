d2a13d13621649241fdce740a55e4727812b1c81
Im-Hassan-wd <alhassansahih@gmail.com>
2023-05-26 19:18:41 +0100

    change icon in the navbar option component

--- src/components/NavbarOption.js
+++ src/components/NavbarOption.js
@@ -6,42 +6,16 @@ export default function NavbarOption() {
   return (
     <div className="navbar-option">
       <button className="action">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"
-          />
-        </svg>
+        <i className="fi fi-ss-bell"></i>
       </button>
       {!isPending && (
         <button className="action" onClick={logout}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
-            />
-          </svg>
+          <i className="fi fi-br-sign-out-alt"></i>
         </button>
       )}
       {isPending && (
         <button className="action" disabled>
-          ...
+          <i className="fi fi-rr-menu-dots"></i>
         </button>
       )}
     </div>
