28ba5b2170933f764326e571848d32b6bf51987f
huzitaiwo <alhassansahih@gmail.com>
2023-05-31 23:41:31 +0100

    navbar

--- src/components/NavbarOption.js
+++ src/components/NavbarOption.js
@@ -1,23 +1,12 @@
-import { useLogout } from "../hooks/useLogout";
-
 export default function NavbarOption() {
-  const { logout, isPending } = useLogout();
-
   return (
     <div className="navbar-option">
       <button className="action">
-        <i className="fi fi-sr-bell"></i>
+        <i className="fi fi-sr-messages"></i>
       </button>
-      {!isPending && (
-        <button className="action" onClick={logout}>
-          <i className="fi fi-sr-sign-out-alt"></i>
-        </button>
-      )}
-      {isPending && (
-        <button className="action" disabled>
-          <i className="fi fi-sr-menu-dots"></i>
-        </button>
-      )}
+      <button className="action">
+        <i className="fi fi-sr-bells"></i>
+      </button>
     </div>
   );
 }
