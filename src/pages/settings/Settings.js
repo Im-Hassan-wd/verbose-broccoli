adc46096fe11b9d865e9e7006bea57092b8897bb
huzitaiwo <alhassansahih@gmail.com>
2023-06-01 21:15:24 +0100

    settings

--- src/pages/settings/Settings.js
+++ src/pages/settings/Settings.js
@@ -3,11 +3,24 @@ import "./Settings.css";
 
 //components
 import ThemeSelector from "../../components/ThemeSelector";
+import { useLogout } from "../../hooks/useLogout";
 
 export default function Settings() {
+  const { logout, isPending, error } = useLogout();
+
   return (
     <div className="settings">
       <ThemeSelector />
+      {!isPending && (
+        <button className="btn" onClick={logout}>
+          Logout
+        </button>
+      )}
+      {isPending && (
+        <button className="btn" disabled>
+          Logging out...
+        </button>
+      )}
     </div>
   );
 }
