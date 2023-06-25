13cb94a594d891955805e2a553638dba20131c4f
huzitaiwo <alhassansahih@gmail.com>
2023-06-25 22:55:23 +0100

    loader componenst

---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+// styles
+import "./Loader.css";
+
+export default function Loader() {
+  return (
+    <div class="loading-container">
+      <div class="loader"></div>
+      <p class="loading-text">Loading...</p>
+    </div>
+  );
+}
